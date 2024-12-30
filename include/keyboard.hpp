9f222f2baeca658768854f37069ae6e085bcde37
stdnullptr <antonio.liubchev@gmail.com>
2024-12-30 16:23:43 +0200

    feat: extend keyboard utils

--- include/keyboard.hpp
+++ include/keyboard.hpp
@@ -4,8 +4,15 @@
 
 namespace commons::keyboard
 {
+    // Check if the key is currently pressed
     inline bool is_key_pressed(const int virtual_key)
     {
         return (GetAsyncKeyState(virtual_key) & 0x8000) != 0;
+    }
+
+    // Check if the key was pressed since the last check
+    inline bool was_key_pressed(const int virtual_key)
+    {
+        return (GetAsyncKeyState(virtual_key) & 0x1) != 0;
     }
 }
